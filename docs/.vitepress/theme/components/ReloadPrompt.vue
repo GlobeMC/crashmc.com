bd052389459e4936ca247762fc7abed6b5b99bb0
Big-Cake-jpg <2577117205@qq.com>
2023-08-01 11:57:36 +0800

    我的上帝 我居然丢了弹窗的样式

--- docs/.vitepress/theme/components/ReloadPrompt.vue
+++ docs/.vitepress/theme/components/ReloadPrompt.vue
@@ -1,24 +1,24 @@
 <script setup lang="ts">
-import { onBeforeMount, ref } from 'vue'
+import { onBeforeMount, ref } from "vue";
 
-const needRefresh = ref(false)
+const needRefresh = ref(false);
 
-let updateServiceWorker: (() => Promise<void>) | undefined
+let updateServiceWorker: (() => Promise<void>) | undefined;
 
 function onNeedRefresh() {
-  needRefresh.value = true
+  needRefresh.value = true;
 }
 async function close() {
-  needRefresh.value = false
+  needRefresh.value = false;
 }
 
 onBeforeMount(async () => {
-  const { registerSW } = await import('virtual:pwa-register')
+  const { registerSW } = await import("virtual:pwa-register");
   updateServiceWorker = registerSW({
     immediate: true,
     onNeedRefresh,
-  })
-})
+  });
+});
 </script>
 
 <template>
@@ -48,3 +48,29 @@ onBeforeMount(async () => {
     </div>
   </template>
 </template>
+
+<style>
+.pwa-toast {
+  position: fixed;
+  right: 0;
+  bottom: 0;
+  margin: 16px;
+  padding: 12px;
+  border: 1px solid #8885;
+  border-radius: 4px;
+  z-index: 100;
+  text-align: left;
+  box-shadow: 3px 4px 5px 0 #8885;
+  background-color: white;
+}
+.pwa-toast #pwa-message {
+  margin-bottom: 8px;
+}
+.pwa-toast button {
+  border: 1px solid #8885;
+  outline: none;
+  margin-right: 5px;
+  border-radius: 2px;
+  padding: 3px 10px;
+}
+</style>
