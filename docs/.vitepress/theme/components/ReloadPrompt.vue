7acb1ba3575bdc9f4d75b276e9ad1965c4a4932d
Big-Cake-jpg <2577117205@qq.com>
2023-08-01 23:12:52 +0800

    让我们修一修这个 PWA 的弹窗吧

--- docs/.vitepress/theme/components/ReloadPrompt.vue
+++ docs/.vitepress/theme/components/ReloadPrompt.vue
@@ -61,7 +61,6 @@ onBeforeMount(async () => {
   z-index: 100;
   text-align: left;
   box-shadow: 3px 4px 5px 0 #8885;
-  background-color: white;
 }
 .pwa-toast #pwa-message {
   margin-bottom: 8px;
