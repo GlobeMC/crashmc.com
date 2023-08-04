30e4c27a6a2b86b1b9774296c9416aeb6a568a3e
HRxiaohu <HRxiaohu1@163.com>
2023-08-04 21:45:43 +0800

    📃 docs: 崩溃提交 ISSUE 模板

--- components/analyzer.vue
+++ components/analyzer.vue
@@ -165,11 +165,13 @@ function StartAnalysis(file, ext) {
   }
 }
 function LogAnalysis(log) {
-  console.warn(launcher);
-  console.log(log);
-  ShowAnalysisResult('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')
+  if(''){}
+  else{
+    ShowAnalysisResult('本工具还未收录您所遇到的错误，请点击下方按钮前往 Github 反馈。','https://github.com/GlobeMC/crashmc.com/issues/new')
+  }
 }
-function ShowAnalysisResult(msg) {
+function ShowAnalysisResult(msg,result_url) {
+  redirect_url.value = redirect_url;
   document.getElementById('analysis_result_main').style.display = 'block';
   document.getElementById('analysis_result_msg').innerText = msg;
 
