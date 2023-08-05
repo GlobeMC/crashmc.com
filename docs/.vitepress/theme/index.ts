890a9592bb192486fd1488a4f24af6a9842923ed
Big-Cake-jpg <2577117205@qq.com>
2023-08-05 20:12:16 +0800

    我看看这能不能修好

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -2,18 +2,18 @@ import { h } from "vue";
 import Theme from "vitepress/theme";
 import "./style.css";
 import comment from "../../../components/comment.vue";
-import Author from "./components/Author.vue"
-import ReloadPrompt from './components/ReloadPrompt.vue'
+import Author from "./components/Author.vue";
+import ReloadPrompt from "./components/ReloadPrompt.vue";
 
 export default {
-	...Theme,
-	Layout: () => {
-		return h(Theme.Layout, null, {
-			// https://vitepress.dev/guide/extending-default-theme#layout-slots
+  ...Theme,
+  Layout: () => {
+    return h(Theme.Layout, null, {
+      // https://vitepress.dev/guide/extending-default-theme#layout-slots
       "doc-after": () => h(comment),
-      "doc-footer-before": () => h(Author),
-			'layout-bottom': () => h(ReloadPrompt),
-		});
-	},
-	// ...
+      // "doc-footer-before": () => h(Author),
+      "layout-bottom": () => h(ReloadPrompt),
+    });
+  },
+  // ...
 };
