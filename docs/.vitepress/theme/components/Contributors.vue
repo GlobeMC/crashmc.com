f9cf63c478770747130ff0732161349902a0dff7
bingling-sama <bingling_sama@qq.com>
2023-08-06 14:03:36 +0800

    🐞 fix(Contributor): fix refresh
    fix contributor refresh when route updated
    move contributor data to vitepress theme

--- docs/.vitepress/theme/components/Contributors.vue
+++ docs/.vitepress/theme/components/Contributors.vue
@@ -1,35 +1,35 @@
 <script setup lang="ts">
-  import { useData } from "vitepress";
-import { ref } from "vue";
+import { useData } from "vitepress";
+import { computed } from "vue";
+import members from "../../data/members"
 
-  const defaultAuthor = "GlobeMC"
-  const { frontmatter } = useData()
+const defaultAuthor = "GlobeMC"
+const { frontmatter } = useData()
 
-  const contributorsArr = [
+const contributors = computed(() => {
+  return [
     frontmatter.value?.author,
     ...(frontmatter.value.contributors || []),
   ].filter((x) => x)
-  const contributors = ref(contributorsArr)
+})
 
-  function getAvatarUrl(name: string) {
-    return `https://github.com/${name}.png`
-  }
-  function getGithubLink(name: string) {
-    return `https://github.com/${name}`
-  }
+function getAvatarUrl(name: string) {
+  return members.find((member) => { return member.name === name })?.avatar
+}
 
-  function isNotEmpty(arr: string | string[]) {
-    return Array.isArray(arr) && arr.length
-  }
+function getGithubLink(name: string) {
+  return `https://github.com/${name}`
+}
+
+function isNotEmpty(arr: string | string[]) {
+  return Array.isArray(arr) && arr.length
+}
 </script>
 
 <template>
   <p class="vp-main-color con">本文贡献者:</p>
   <div v-if="isNotEmpty(contributors)" class="flex flex-wrap gap-4">
-    <div
-      v-for="contributor of contributors"
-      :key="contributor"
-      class="flex gap-2 items-center vp-main-color">
+    <div v-for="contributor of contributors" :key="contributor" class="flex gap-2 items-center vp-main-color">
       <a :href="getGithubLink(contributor)" rel="noreferrer" target="_blank">
         <img :src="getAvatarUrl(contributor)" class="w-8 h-8 rounded-full" />
       </a>
@@ -45,45 +45,53 @@ import { ref } from "vue";
 </template>
 
 <style scoped>
-  .flex {
-    display: flex;
-  }
-  .flex-warp {
-    flex-wrap: wrap;
-  }
-  .gap-2 {
-    grid-gap: 0.5rem;
-    gap: 0.5rem;
-  }
-  .gap-4 {
-    grid-gap: 1rem;
-    gap: 1rem;
-  }
-  .items-center {
-    align-items: center;
-  }
-  .w-8 {
-    width: 2rem;
-  }
-  .h-8 {
-    width: 2rem;
-  }
-  .rounded-full {
-    border-radius: 9999px;
-  }
+.flex {
+  display: flex;
+}
 
-  img {
-    display: block;
-    border: 0.1px solid var(--vp-c-brand);
-  }
+.flex-warp {
+  flex-wrap: wrap;
+}
 
-  p {
-    line-height: 24px;
-    /* font-size: 14px; */
-    font-weight: 500;
-    color: var(--vp-c-brand);
-  }
-  .con{
-    margin-bottom: 6px;
-  }
+.gap-2 {
+  grid-gap: 0.5rem;
+  gap: 0.5rem;
+}
+
+.gap-4 {
+  grid-gap: 1rem;
+  gap: 1rem;
+}
+
+.items-center {
+  align-items: center;
+}
+
+.w-8 {
+  width: 2rem;
+}
+
+.h-8 {
+  width: 2rem;
+}
+
+.rounded-full {
+  border-radius: 9999px;
+}
+
+img {
+  display: block;
+  border: 0.1px solid var(--vp-c-brand);
+}
+
+p {
+  line-height: 24px;
+  /* font-size: 14px; */
+  font-weight: 500;
+  color: var(--vp-c-brand);
+}
+
+.con {
+  margin-bottom: 6px;
+}
 </style>
