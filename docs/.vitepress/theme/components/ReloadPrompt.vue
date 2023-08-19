358d5ed0b72726fbc39e2d1bc1847fc0fd159520
Kevin Z <zyxkad@gmail.com>
2023-08-18 19:52:32 -0600

    add animation for the pwa refresh prompt (#137)

--- docs/.vitepress/theme/components/ReloadPrompt.vue
+++ docs/.vitepress/theme/components/ReloadPrompt.vue
@@ -4,15 +4,22 @@ import { onBeforeMount, ref } from "vue"
 const offlineReady = ref(false)
 const needRefresh = ref(false)
 
-let updateServiceWorker: (() => Promise<void>) | undefined
+var updateServiceWorker: (() => Promise<void>) | undefined
 
-const onOfflineReady = () => {
+function onOfflineReady() {
   offlineReady.value = true
 }
+
 function onNeedRefresh() {
   needRefresh.value = true
 }
-async function close() {
+
+function refresh() {
+  needRefresh.value = false
+  updateServiceWorker?.()
+}
+
+function close() {
   offlineReady.value = false
   needRefresh.value = false
 }
@@ -28,31 +35,33 @@ onBeforeMount(async () => {
 </script>
 
 <template>
-  <template v-if="needRefresh">
-    <div
-      class="pwa-toast z-100 bg-$vp-c-bg border border-$pwa-divider fixed right-0 bottom-0 m-6 px-6 py-4 rounded shadow-xl"
-      role="alertdialog"
-      aria-labelledby="pwa-message">
-      <div id="pwa-message" class="mb-3">
-        发现新内容可用，点击“刷新”按钮来更新文档
+  <Transition name="pwa-hint">
+    <template v-if="needRefresh">
+      <div
+        class="pwa-toast z-100 bg-$vp-c-bg border border-$pwa-divider fixed right-0 bottom-0 m-6 px-6 py-4 rounded shadow-xl"
+        role="alertdialog"
+        aria-labelledby="pwa-message">
+        <div id="pwa-message" class="mb-3">
+          发现新内容可用，点击“刷新”按钮来更新文档
+        </div>
+        <button
+          type="button"
+          class="pwa-refresh mr-2 px-3 py-1 rounded"
+          @click="refresh">
+          刷新
+        </button>
+        <button
+          type="button"
+          class="pwa-cancel border border-$pwa-divider mr-2 px-3 py-1 rounded"
+          @click="close">
+          取消
+        </button>
       </div>
-      <button
-        type="button"
-        class="pwa-refresh mr-2 px-3 py-1 rounded"
-        @click="updateServiceWorker?.()">
-        刷新
-      </button>
-      <button
-        type="button"
-        class="pwa-cancel border border-$pwa-divider mr-2 px-3 py-1 rounded"
-        @click="close">
-        取消
-      </button>
-    </div>
-  </template>
+    </template>
+  </Transition>
 </template>
 
-<style>
+<style scoped>
 .pwa-toast {
   position: fixed;
   right: 0;
@@ -66,9 +75,11 @@ onBeforeMount(async () => {
   box-shadow: 3px 4px 5px 0 #8885;
   background-color: var(--vp-c-bg-soft);
 }
+
 .pwa-toast #pwa-message {
   margin-bottom: 8px;
 }
+
 .pwa-toast button {
   border: 1px solid #8885;
   outline: none;
@@ -76,37 +87,55 @@ onBeforeMount(async () => {
   border-radius: 2px;
   padding: 3px 10px;
 }
+
 .pwa-toast .pwa-refresh {
   border-color: var(--vp-button-brand-border);
   color: var(--vp-button-brand-text);
   background-color: var(--vp-button-brand-bg);
 }
+
 .pwa-toast .pwa-refresh:hover {
   border-color: var(--vp-button-brand-hover-border);
   color: var(--vp-button-brand-hover-text);
   background-color: var(--vp-button-brand-hover-bg);
 }
+
 .pwa-toast .pwa-refresh:active {
   border-color: var(--vp-button-brand-active-border);
   color: var(--vp-button-brand-active-text);
   background-color: var(--vp-button-brand-active-bg);
 }
+
 .pwa-toast .pwa-cancel {
   border-color: var(--vp-button-alt-border);
   color: var(--vp-button-alt-text);
   background-color: var(--vp-button-alt-bg);
 }
+
 .pwa-toast .pwa-cancel:hover {
   border-color: var(--vp-button-alt-hover-border);
   color: var(--vp-button-alt-hover-text);
   background-color: var(--vp-button-alt-hover-bg);
 }
+
 .pwa-toast .pwa-cancel:active {
   border-color: var(--vp-button-alt-active-border);
   color: var(--vp-button-alt-active-text);
   background-color: var(--vp-button-alt-active-bg);
 }
+
 .dark .pwa-toast {
   --pwa-divider: var(--vp-c-divider-dark-1);
 }
+
+.pwa-hint-enter-active,
+.pwa-hint-leave-active {
+  transition: all 0.5s ease;
+}
+
+.pwa-hint-enter-from,
+.pwa-hint-leave-to {
+  right: -100% !important;
+}
+
 </style>
