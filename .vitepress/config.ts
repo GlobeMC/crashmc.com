453dc959361d22cdb03f38129126ce6edc32b4c7
Big-Cake-jpg <2577117205@qq.com>
2024-05-02 13:09:27 +0800

    修复可能导致构建失败的问题

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -38,6 +38,7 @@ const viteConfig = {
 		noExternal: [
 			// 如果还有别的依赖需要添加的话，并排填写和配置到这里即可
 			'@nolebase/vitepress-plugin-enhanced-readabilities',
+			'@nolebase/vitepress-plugin-inline-link-preview',
 		],
 	},
 	plugins: [
