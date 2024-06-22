4d56b8bc93ab2c853f160878c25d80aad0c885cf
bingling-sama <bingling_sama@qq.com>
2024-06-22 18:40:31 +0800

    ✨ feat(blog): create blog part

---
+++ .vitepress/theme/layouts/Posts.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+import { data as posts } from "../../utils/posts.data"
+
+</script>
+
+<template>
+	<div v-for="post in posts" :key="post.url">
+		<a :href="post.url">
+			{{ post.title }}
+		</a>
+		<!-- <div v-html="post.excerpt"></div> -->
+	</div>
+</template>
+
+<style scoped></style>
---
+++ docs/blog/post1.md
@@ -0,0 +1,15 @@
+---
+sidebar: false
+title: Post1
+date: 2021-01-01T00:00:00Z
+---
+
+# Post1
+
+This is the first post.
+
+---
+
+Some text here.
+
+This is the end of the first post.
